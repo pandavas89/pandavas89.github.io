2b4584b03bdc210885e7f944222c6aed2649d5e6
사민철 <pandavas89@gmail.com>
2023-06-07 16:48:03 +0900

    ✨ Initial Commit

---
+++ pelicanconf.py
@@ -0,0 +1,32 @@
+AUTHOR = 'pandavas89'
+SITENAME = 'PlayEng Ground'
+SITEURL = 'http://blog.tykl.me'
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Seoul'
+
+DEFAULT_LANG = 'KR'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'https://getpelican.com/'),
+         ('Python.org', 'https://www.python.org/'),
+         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (
+    ('Instagram', 'https://www.instagram.com/pandavas89/'),
+)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
